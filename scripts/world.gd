31497bc2621f1005894ba9abb5999606de7c840a
flavor-savior <monoglyph@proton.me>
2026-02-02 22:57:10 -0500

    Update world.gd
    reverting to main branch version, pending menu reimplementation

--- scripts/world.gd
+++ scripts/world.gd
@@ -15,9 +15,6 @@ var time_since_last_spawn: float = 0.0
 
 var mob_fragments: int = 0
 
-# this is probably jank and bad, but this conditional stops the game from running until the menu node toggles it
-var playing: bool = false
-
 func _ready():
 	ground = get_node("Ground")
 	walls = get_node("Walls")
@@ -27,38 +24,6 @@ func _set_instances():
 	greaser_spawns = get_tree().get_nodes_in_group("greaser_spawn")
 	player_instance = get_tree().get_first_node_in_group("player")
 	ySort = get_node("/root/Game/World/YSort")
-
-# Find all the "GreaserSpawn" nodes and get their global position
-var greaser_spawns: Array[Node] = []
-var max_greasers: int = 50
-var cooldown: float = 1.0
-var time_since_last_spawn: float = 0.0
-
-func _process(delta: float):
-	if playing == true:
-		print_debug_info()
-		var greasers := get_tree().get_nodes_in_group("greaser")
-		if greasers.size() < max_greasers and time_since_last_spawn > cooldown:
-			_spawn_greaser()
-			time_since_last_spawn = 0.0
-		time_since_last_spawn += delta
-	
-# Spawn a greaser at a random greaser spawn
-func _spawn_greaser():
-	var random_spawn := greaser_spawns[randi() % greaser_spawns.size()]
-	random_spawn.spawn_greaser()
-
-func print_debug_info():
-	var greasers := get_tree().get_nodes_in_group("greaser")
-	debug_label.text = "Current Greasers: " + str(greasers.size()) + \
-		"\nFrags: 0"
-
-func unleash_player() ->void:
-	get_node("Character").max_speed = 400.0
-	
-func _on_play_btn_button_up() -> void:
-	unleash_player()
-	playing = true
 
 func prepare_for_wave():
 	player_instance.global_position = PLAYER_START_POSITION
